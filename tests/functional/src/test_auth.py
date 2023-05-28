d8bd81cc21b7409c1acd9113ac8f4c70542951b4
Elena Balakhonova <balakhonova.e.v@yandex.ru>
2023-05-28 14:14:56 +0300

    Complete auth endpoint tests

--- tests/functional/src/test_auth.py
+++ tests/functional/src/test_auth.py
@@ -1,6 +1,7 @@
 import pytest
-from flask import json
 from http import HTTPStatus
+
+from flask import json
 
 from src.app import app
 from src.db.db_config import db_session
@@ -18,6 +19,7 @@ from src.logs.log_config import logger
                 'email': 'test@email.com',
                 'name': 'Testen',
                 'surname': 'Testenson',
+                'role': 'user',
             },
             {
                 'status': HTTPStatus.OK,
@@ -39,15 +41,15 @@ from src.logs.log_config import logger
 )
 def test_registration(data, expected_answer):
     tester = app.test_client()
-
     logger.info('DATA: %s', data)
-
-    response = tester.post('api/v1/auth/register', data=json.dumps(data), content_type='application/json')
+    response = tester.post(
+        'api/v1/auth/register',
+        data=json.dumps(data),
+        content_type='application/json',
+    )
     response_data = json.loads(response.data)
-    
     assert response.status_code == expected_answer['status']
     assert response_data['message'] == expected_answer['message']
-    
     with app.app_context():
         test_user = db_session.query(User).filter_by(username='test_user').first()
         if test_user:
@@ -87,18 +89,149 @@ def test_login(data, expected_answer):
         'password': 'ivanov666',
         'email': 'ivan@ivanov.com',
     }
-    response = tester.post('api/v1/auth/register', data=json.dumps(new_user), content_type='application/json')
-    
+    response = tester.post(
+        'api/v1/auth/register',
+        data=json.dumps(new_user),
+        content_type='application/json',
+    )
     logger.info('Just created user: %s', new_user)
-
     logger.info('DATA: %s', data)
-
-    response = tester.post('api/v1/auth/login', data=json.dumps(data), content_type='application/json')
+    response = tester.post(
+        'api/v1/auth/login',
+        data=json.dumps(data),
+        content_type='application/json',
+    )
     response_data = json.loads(response.data)
-    
+    assert response.status_code == expected_answer['status']
+    if response.status_code == HTTPStatus.OK:
+        assert response_data['access_token'] is not None
+        assert response_data['refresh_token'] is not None
+    else:
+        assert response_data['message'] == expected_answer['message']
+    with app.app_context():
+        test_user = db_session.query(User).filter_by(username='ivan.ivanov').first()
+        if test_user:
+            db_session.delete(test_user)
+            db_session.commit()
+            logger.info('Deleted user %s', test_user)
+
+
+@pytest.mark.parametrize(
+    'data, expected_answer',
+    [
+        (
+            {
+                'new_password': 'newP@ssworD666',
+            },
+            {
+                'status': HTTPStatus.OK,
+                'message': 'Password reset successfully'
+            },
+        ),
+    ],
+)
+def test_password_reset(data, expected_answer):
+    tester = app.test_client()
+    new_user = {
+        'username': 'ivan.ivanov',
+        'password': 'ivanov666',
+        'email': 'ivan@ivanov.com',
+    }
+    response = tester.post(
+        'api/v1/auth/register',
+        data=json.dumps(new_user),
+        content_type='application/json',
+    )
+    logger.info('Just created user: %s', new_user)
+    response = tester.post(
+        'api/v1/auth/login',
+        data=json.dumps({
+            'username': 'ivan.ivanov',
+            'password': 'ivanov666',
+        }),
+        content_type='application/json',
+    )
+    response_data = json.loads(response.data)
+    assert response.status_code == HTTPStatus.OK
+    assert response_data['access_token'] is not None
+    assert response_data['refresh_token'] is not None
+    response = tester.post(
+        'api/v1/auth/password-reset',
+        data=json.dumps({
+            'new_password': data['new_password'],
+        }),
+        content_type='application/json',
+        headers={'Authorization': f'Bearer {response_data["access_token"]}'},
+    )
+    response_data = json.loads(response.data)
     assert response.status_code == expected_answer['status']
     assert response_data['message'] == expected_answer['message']
+    response = tester.post(
+        'api/v1/auth/login',
+        data=json.dumps({
+            'username': 'ivan.ivanov',
+            'password': data['new_password'],
+        }),
+        content_type='application/json',
+    )
+    response_data = json.loads(response.data)
+    assert response.status_code == HTTPStatus.OK
+    assert response_data['access_token'] is not None
+    assert response_data['refresh_token'] is not None
+    with app.app_context():
+        test_user = db_session.query(User).filter_by(username='ivan.ivanov').first()
+        if test_user:
+            db_session.delete(test_user)
+            db_session.commit()
+            logger.info('Deleted user %s', test_user)
 
+
+def test_logout():
+    tester = app.test_client()
+    new_user = {
+        'username': 'ivan.ivanov',
+        'password': 'ivanov666',
+        'email': 'ivan@ivanov.com',
+    }
+    response = tester.post(
+        'api/v1/auth/register',
+        data=json.dumps(new_user),
+        content_type='application/json',
+    )
+    logger.info('Just created user: %s', new_user)
+    response = tester.post(
+        'api/v1/auth/login',
+        data=json.dumps({
+            'username': 'ivan.ivanov',
+            'password': 'ivanov666',
+        }),
+        content_type='application/json',
+    )
+    response_data = json.loads(response.data)
+    assert response.status_code == HTTPStatus.OK
+    assert response_data['access_token'] is not None
+    assert response_data['refresh_token'] is not None
+    access_token = response_data['access_token']
+    response = tester.post(
+        'api/v1/auth/logout',
+        headers={'Authorization': f'Bearer {access_token}'},
+    )
+    response_data = json.loads(response.data)
+    assert response.status_code == HTTPStatus.OK
+    assert response_data['message'] == 'Logout successful, access_token revoked'
+    response = tester.post(
+        'api/v1/auth/login',
+        data=json.dumps({
+            'username': 'ivan.ivanov',
+            'password': 'ivanov666',
+        }),
+        content_type='application/json',
+    )
+    response_data = json.loads(response.data)
+    assert response.status_code == HTTPStatus.OK
+    assert response_data['access_token'] is not None
+    assert response_data['refresh_token'] is not None
+    assert response_data['access_token'] != access_token
     with app.app_context():
         test_user = db_session.query(User).filter_by(username='ivan.ivanov').first()
         if test_user:
